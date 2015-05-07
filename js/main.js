fbca67b1d10064c00701fee3794b0bbf4885746b
Erik Farmer <erikdfarmer@gmail.com>
2015-05-06 19:58:41 -0700

    modify js tags

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 var nameplate = document.getElementsByClassName('name-plate')[0];
 var profile_pic = document.getElementsByClassName('profile-img')[0];
 TweenLite.from(nameplate, 2, {opacity:0});
-TweenLite.from(profile_pic, 1, {opacity:0, right:"200px"});
+TweenLite.from(profile_pic, 1, {opacity:0, right:"150px"});
