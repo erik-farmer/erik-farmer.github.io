40e55b2a9ce7c18881fb3310d719dc34c930f77c
erik-farmer <erikdfarmer@gmail.com>
2017-01-16 12:45:08 -0800

    remove animation

--- js/main.js
+++
@@ -1,4 +0,0 @@
-var nameplate = document.getElementsByClassName('name-plate')[0];
-var profile_pic = document.getElementsByClassName('profile-img')[0];
-TweenLite.from(nameplate, 2, {opacity:0});
-TweenLite.from(profile_pic, 1, {opacity:0, right:"150px"});
