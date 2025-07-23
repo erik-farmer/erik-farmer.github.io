cf12c11d0cf41403d161d74f446ec3b670cd310a
mini-me <mini-me@mini-mes-Mini.attlocal.net>
2025-07-22 21:04:17 -0700

    Fix dark mode. Yeesh

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,20 @@ export default {
   content: [
     "./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte,md,mdx}",
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      typography: (theme) => ({
+        dark: {
+          css: {
+            color: theme('colors.gray.200'),
+            a: { color: theme('colors.blue.400') },
+            strong: { color: theme('colors.gray.100') },
+            'blockquote p': { color: theme('colors.gray.300') },
+          },
+        },
+      }),
+    },
   },
   plugins: [require('@tailwindcss/typography')],
 };
